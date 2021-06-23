5ffac590f949712dec7e7e69d63cd1e0bf192cb7
tarasovc47 <tarasovc47@live.ru>
2021-06-23 22:07:52 +0500

    вынес блок "категории" в отдельный вид
    и подключил его в индексном файле и в странице товара, предварительно изменим контроллеры

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\controllers;
 
+use app\models\Brand;
+use app\models\Category;
 use app\models\Product;
 use Yii;
 use yii\web\Controller;
@@ -10,7 +12,19 @@ class ProductController extends Controller
     public function actionView($id)
     {
         $id = Yii::$app->request->get('id');
+        $brands = Brand::getAll();
         $product = Product::findOne($id);
-        return $this->render('view', compact('product'));
+        $products = Product::find()->where(['hidden' => '0'])->all();
+        $products_min = Product::find()->min('cost');
+        $products_max = Product::find()->max('cost');
+        $categories = Category::getAll();
+        return $this->render('view', [
+            'brands' => $brands,
+            'items' => $products,
+            'products_cost_min' => $products_min,
+            'products_cost_max' => $products_max,
+            'categories' => $categories,
+            'product' => $product,
+        ]);
     }
 }
