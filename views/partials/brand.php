6ff2a36cbf20c0d4b9fadccd7b9188bb03202581
tarasovc47 <tarasovc47@live.ru>
2021-06-26 19:54:33 +0500

    боковая панелька категорий и брендов
    отрисовка боковых панелек брендов вынесена также в отдельный вид, как и категории, в брендах добавлено отображение количества товаров данного бренда

---
+++ views/partials/brand.php
@@ -0,0 +1,23 @@
+<?php
+
+use app\models\Product;
+use yii\helpers\Url;
+
+?>
+
+<div class="brands_products">
+    <h2>Бренды</h2>
+    <div class="brands-name">
+        <ul class="nav nav-pills nav-stacked">
+            <?php
+            foreach ($brands as $brand): ?>
+                <li><a href="<?= Url::to(['brands/view', 'id' => $brand->id]) ?>">
+                    <span class="pull-right">(
+                        <?php
+                        Product::brandCount($brand->id);
+                        ?> )
+                    </span><span class="pull-right"></span><?= $brand->title ?></a></li>
+            <?php endforeach;?>
+        </ul>
+    </div>
+</div>
