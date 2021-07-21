605cc01a25d5b2226012ee07206b15fe3e4c48b7
tarasovc47 <tarasovc47@live.ru>
2021-07-21 15:10:13 +0500

    Добавлена уникальность полям логин, е-майл и телефон таблицы users

---
+++ migrations/m210721_095420_modify_users_table_unique.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210721_095420_modify_users_table_unique
+ */
+class m210721_095420_modify_users_table_unique extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->alterColumn('users', 'login', $this->string()->unique());
+        $this->alterColumn('users', 'email', $this->string()->unique());
+        $this->alterColumn('users', 'phone', $this->integer()->unique());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->alterColumn('users', 'login', $this->string());
+        $this->alterColumn('users', 'email', $this->string());
+        $this->alterColumn('users', 'phone', $this->integer());
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210721_095420_modify_users_table_unique cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
